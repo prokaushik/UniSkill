afb7afbbcc550c55bae254388b2266c9735b237d
prince <prince.dubia03@gmail.com>
2024-04-01 12:12:50 +0530

    Companies

--- src/components/home/location/Location.jsx
+++ src/components/home/location/Location.jsx
@@ -8,7 +8,7 @@ const Location = () => {
     <>
       <section className='location padding'>
         <div className='container'>
-          <Heading title='Explore By Location' subtitle='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.' />
+          <Heading title='Experts From Companies' subtitle='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.' />
 
           <div className='content grid3 mtop'>
             {location.map((item, index) => (
