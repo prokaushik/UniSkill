2fac6eec2ca6529f071996cf15a8994bf678ecf7
prince <prince.dubia03@gmail.com>
2024-04-01 11:41:01 +0530

    Team update

--- src/components/home/team/Team.jsx
+++ src/components/home/team/Team.jsx
@@ -8,12 +8,12 @@ const Team = () => {
     <>
       <section className='team background'>
         <div className='container'>
-          <Heading title='Our Featured Agents' subtitle='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.' />
+          <Heading title='Our Team' subtitle='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.' />
 
           <div className='content mtop grid3'>
             {team.map((val, index) => (
               <div className='box' key={index}>
-                <button className='btn3'>{val.list} Listings</button>
+                
                 <div className='details'>
                   <div className='img'>
                     <img src={val.cover} alt='' />
